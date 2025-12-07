aa2665d21cda7b35a395d50ba8c92867cff353e0
nandani <radhnandani.prasad29@gmail.com>
2025-12-07 14:13:01 +0530

    Create init.py

---
+++ src/init.py
@@ -0,0 +1,13 @@
+"""
+Music Genre Classification Package
+==================================
+
+This package contains modules for:
+- Data preprocessing
+- Feature extraction
+- Model training
+- Prediction
+"""
+
+__version__ = "1.0.0"
+__author__ = "---"
